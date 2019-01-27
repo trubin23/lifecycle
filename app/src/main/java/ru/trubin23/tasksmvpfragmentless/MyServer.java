740581a8dec821f8d6ee1e935b2a8943ef47f525
Trubin Andrey <trubin23@gmail.com>
2019-01-27 22:10:47 +0500

    add LifecycleObserver for MainActivity

---
+++ app/src/main/java/ru/trubin23/tasksmvpfragmentless/MyServer.java
@@ -0,0 +1,27 @@
+package ru.trubin23.tasksmvpfragmentless;
+
+import android.arch.lifecycle.Lifecycle;
+import android.arch.lifecycle.LifecycleObserver;
+import android.arch.lifecycle.LifecycleOwner;
+import android.arch.lifecycle.OnLifecycleEvent;
+import android.util.Log;
+
+public class MyServer implements LifecycleObserver {
+
+    private static final String TAG = MyServer.class.getSimpleName();
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+    public void connect() {
+        //Log.d(TAG, "Lifecycle.Event.ON_START");
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+    public void disconnect() {
+        //Log.d(TAG, "Lifecycle.Event.ON_STOP");
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_ANY)
+    public void onAny(LifecycleOwner source, Lifecycle.Event event) {
+        Log.d(TAG, "Lifecycle.Event.ON_ANY: " + event);
+    }
+}
