b9d8782e1320b8a97dfb0587b8dba0d6b5d7007d
Trubin Andrey <trubin23@gmail.com>
2019-01-29 23:06:22 +0500

    finish implementation MyThread in third_activity package

--- app/src/main/java/ru/trubin23/lifecycle/third_activity/DataSource.java
+++ app/src/main/java/ru/trubin23/lifecycle/third_activity/DataSource.java
@@ -3,9 +3,24 @@ package ru.trubin23.lifecycle.third_activity;
 
 import android.arch.lifecycle.MutableLiveData;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 class DataSource {
 
     private static DataSource INSTANCE;
+
+    private static final int THREAD_COUNT = 5;
+
+    private List<MyThread> mMyThreadList = new ArrayList<>();
+    private String[] mDescriptionArray = {
+            "Первый:",
+            "Второй:",
+            "Третий:",
+            "Четвертый:",
+            "Пятый:"
+    };
 
     private DataSource() {
     }
@@ -14,36 +29,10 @@ class DataSource {
         if (INSTANCE == null) {
             INSTANCE = new DataSource();
 
-
-
-        }
-        return INSTANCE;
-    }
-
-    private class MyThread extends Thread{
-
-        private MutableLiveData mMutableLiveData;
-        private String mDescription;
-        private int mSleepTime;
-
-        MyThread(MutableLiveData<String> mutableLiveData, String description, int sleepTime){
-            mMutableLiveData = mutableLiveData;
-            mDescription = description;
-            mSleepTime = sleepTime;
-        }
-
-        @Override
-        public void run() {
-            super.run();
-            while (true) {
-                mMutableLiveData.postValue(mDescription);
-
-                try {
-                    Thread.sleep(mSleepTime);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+            for (int i=0;i<THREAD_COUNT;i++){
+                //MyThread myThread = new MyThread();
             }
         }
+        return INSTANCE;
     }
 }
---
+++ app/src/main/java/ru/trubin23/lifecycle/third_activity/MyThread.java
@@ -0,0 +1,30 @@
+package ru.trubin23.lifecycle.third_activity;
+
+import android.arch.lifecycle.MutableLiveData;
+
+public class MyThread extends Thread {
+
+    private MutableLiveData<String> mMutableLiveData = new MutableLiveData<>();
+    private String mDescription;
+    private int mSleepTime;
+
+    MyThread(String description, int sleepTime) {
+        mDescription = description;
+        mSleepTime = sleepTime;
+    }
+
+    @Override
+    public void run() {
+        super.run();
+
+        for (int index = 0; ; index++) {
+            mMutableLiveData.postValue(mDescription + " " + String.valueOf(index));
+
+            try {
+                Thread.sleep(mSleepTime);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
